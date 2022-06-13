b72c9a8a22d307fdef403e1cdb2e17aef82e5fd2
Lukas Pol <lukaspaespol@gmail.com>
2022-06-13 16:27:19 -0300

    chore(actions): add variable to run rspec with coverage

--- spec/support/coverage.rb
+++ spec/support/coverage.rb
@@ -1,6 +1,6 @@
 require 'simplecov'
 
-SimpleCov.start 'rails' do
+SimpleCov.start 'rails' if ENV['RSPEC_COVERAGE'] do
   minimum_coverage 50
   add_filter %w[app/channels app/jobs app/mailers app/views]
 end
