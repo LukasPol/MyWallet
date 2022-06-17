69294ef6dd3385ec1a4ec68cac95786c9aa6df4f
Lukas Pol <lukaspaespol@gmail.com>
2022-06-17 13:57:13 -0300

    setup(seed): add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Stock.create([
+               { name: 'Petrobrás', code: 'PETR4', value: 0 },
+               { name: 'Petrobrás', code: 'PETR3', value: 0 },
+               { name: 'OI', code: 'OIBR3', value: 0 },
+               { name: 'OI', code: 'OIBR4', value: 0 }
+             ])
+
+User.create(email: 'test@test.com', password: '123456', password_confirmation: '123456')
