5f3011a55fa6cc84eff66800b152737d35bfb26b
Lukas Pol <lukaspaespol@gmail.com>
2022-06-23 12:32:13 -0300

    setup: change migration for to use uuid

--- db/migrate/20220613152211_create_stocks.rb
+++ db/migrate/20220613152211_create_stocks.rb
@@ -1,6 +1,8 @@
 class CreateStocks < ActiveRecord::Migration[7.0]
   def change
-    create_table :stocks do |t|
+    enable_extension 'pgcrypto'
+
+    create_table :stocks, id: :uuid do |t|
       t.string :name
       t.string :code
       t.float :value
