48d83aa7d55ac91eec3a6b990e0b9aae04eb6c85
Lukas Pol <lukaspaespol@gmail.com>
2022-06-23 12:17:50 -0300

    feat(Trading): add pundit in controller

---
+++ app/policies/trading_policy.rb
@@ -0,0 +1,13 @@
+class TradingPolicy < ApplicationPolicy
+  def show?
+    @user == @record.user
+  end
+
+  def update?
+    @user == @record.user
+  end
+
+  def destroy?
+    @user == @record.user
+  end
+end
