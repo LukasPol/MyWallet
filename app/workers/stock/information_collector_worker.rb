e56fde8714ef41f660b1d8a4eded117a87441141
Lukas Pol <lukaspaespol@gmail.com>
2022-06-29 16:46:20 -0300

    feat(Stock::InformationCollectorWorker): run worker only stock exchange is open

--- app/workers/stock/information_collector_worker.rb
+++ app/workers/stock/information_collector_worker.rb
@@ -6,13 +6,26 @@ class Stock
     sidekiq_options queue: :instant_messages, retry: 4
 
     def perform
+      if stocks_exchange_closed?
+        Rails.logger.info('Stock Exchange Closed')
+        return
+      end
+
       Rails.logger.info('Stock::InformationCollectorWorker')
 
       Stock.all.each do |stock|
-        Rails.logger.info("\n === Get information: #{stock} === ")
+        Rails.logger.info("=== Get information: #{stock} ===")
         Stock::InformationCollector.call(stock)
-        sleep rand(5..12)
+        sleep rand(5..10)
       end
+    end
+
+    def stocks_exchange_closed?
+      time_now = Time.zone.now
+      open = Time.zone.parse('10:00:00')
+      close = Time.zone.parse('18:00:00')
+
+      (open > time_now) || (close < time_now)
     end
   end
 end
