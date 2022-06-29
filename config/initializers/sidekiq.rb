19867821fb2f712505fad47e5de180fe43bbd714
Lukas Pol <lukaspaespol@gmail.com>
2022-06-29 15:28:06 -0300

    setup: fix the activejob and add redis in app

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -13,4 +13,9 @@ Sidekiq.configure_client do |config|
   config.redis = { url: (ENV['REDIS_URL']).to_s }
 end
 
-config.active_job.queue_adapter = :sidekiq
+Rails.application.config.cache_store = :redis_cache_store, {
+  url: ENV['REDIS_URL'],
+  network_timeout: 5
+}
+
+Rails.application.config.active_job.queue_adapter = :sidekiq
