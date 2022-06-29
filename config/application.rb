1dc57184c416707a423d9e8c8be672e5bda9799d
Lukas Pol <lukaspaespol@gmail.com>
2022-06-29 17:31:02 -0300

    setup: change time zone brasilia to Sao Paulo

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module MyStocks
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.time_zone = 'Brasilia'
+    config.time_zone = 'America/Sao_Paulo'
 
     config.i18n.load_path += Dir[
       Rails.root.join('config/locales/**/*.{rb,yml}').to_s
