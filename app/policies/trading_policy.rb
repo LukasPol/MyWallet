a89a69552b64aff02b53fec1e7e350a85591e970
Lukas Pol <lukaspaespol@gmail.com>
2022-07-04 16:23:24 -0300

    refactor(Policy): remove method duplicated

--- app/policies/trading_policy.rb
+++ app/policies/trading_policy.rb
@@ -1,13 +1,2 @@
 class TradingPolicy < ApplicationPolicy
-  def show?
-    @user == @record.user
-  end
-
-  def update?
-    @user == @record.user
-  end
-
-  def destroy?
-    @user == @record.user
-  end
 end
