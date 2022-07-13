bbcc2e91431d427abeee6354606ca4feaa79a04a
Lukas Pol <lukaspaespol@gmail.com>
2022-07-13 10:08:55 -0300

    test(Asset): add column `total_invested`

--- spec/models/asset_spec.rb
+++ spec/models/asset_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Asset, type: :model do
   describe 'validates' do
     context 'presence' do
       it { should validate_presence_of(:average_price) }
+      it { should validate_presence_of(:total_invested) }
       it { should validate_presence_of(:amount) }
       it { should validate_presence_of(:user) }
       it { should validate_presence_of(:wallet) }
@@ -13,6 +14,7 @@ RSpec.describe Asset, type: :model do
     context 'validate numericality' do
       it { should validate_numericality_of(:average_price).is_greater_than_or_equal_to(0) }
       it { should validate_numericality_of(:amount).is_greater_than_or_equal_to(0) }
+      it { should validate_numericality_of(:total_invested).is_greater_than_or_equal_to(0) }
     end
   end
 end
