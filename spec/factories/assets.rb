a0077c25fa6fbc2297fe67c7c1b8f71188e1e600
Lukas Pol <lukaspaespol@gmail.com>
2022-07-13 18:00:30 -0300

    test(Asset): initialize Asset with value zero

--- spec/factories/assets.rb
+++ spec/factories/assets.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :asset do
-    amount { 10 }
-    average_price { 10.5 }
+    amount { 0 }
+    average_price { 0 }
+    total_invested { 0 }
     stock { create(:stock) }
     user { create(:user) }
     wallet { user.main_wallet }
